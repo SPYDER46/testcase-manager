b8b74f0cb847eb40b16c6768170f24066dc0a2c2
MUTHU <muthuvelraj2818@gmail.com>
2025-06-09 17:22:15 +0530

    Change titles

--- app.py
+++ app.py
@@ -49,10 +49,9 @@ app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=587,
     MAIL_USE_TLS=True,
-    MAIL_USERNAME='muthuvelraj2818@gmail.com',
-    MAIL_PASSWORD='kxgh igwf elbh xibo'
-    # MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
-    # MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+   
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 )
 
 mail = Mail(app)
