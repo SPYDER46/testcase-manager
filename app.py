ee5aeed507831d959ae586811bdc42fa7c500766
MUTHU <muthuvelraj2818@gmail.com>
2025-06-12 12:05:59 +0530

    UI changes

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ app.config.update(
     MAIL_PORT=587,
     MAIL_USE_TLS=True,
 
+
     MAIL_DEFAULT_SENDER='muthuvelraj2818@gmail.com',
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME'),
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
